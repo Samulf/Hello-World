eeab0bc0bba30b4bf3c78afd336ab7f3f5ed87d1
Samuel Olofsson Jonsson <JonssonSamuel@hotmail.com>
2017-09-11 12:23:29 +0200

    Jag la till mitt namn

--- Hello world app/Program.cs
+++ Hello world app/Program.cs
@@ -11,6 +11,7 @@ namespace Hello_world_app
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Samuel");
         }
     }
 }
